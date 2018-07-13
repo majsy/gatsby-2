f5ff7273139edb9044b27850ff10e4bb7c4e9449
Madeleine <madeleineauvinen@gmail.com>
2018-07-13 15:50:53 -0400

    add react csstransition animation text add ease

--- src/components/home-page/intro/Intro.js
+++ src/components/home-page/intro/Intro.js
@@ -1,33 +1,30 @@
 import React, { Component } from "react";
-// import Transition from 'react-transition-group/Transition';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 import styles from "./intro.module.css";
 import typography from "../../../base/typography.module.css";
 
-// const defaultStyle = {
-//   transition: `opacity 600ms ease-in-out`,
-//   opacity: 0,
-// }
-
-// const transitionStyles = {
-//   entering: { opacity: 0 },
-//   entered:  { opacity: 1 },
-// };
-
 export default class Intro extends Component {
+  state = {
+    showFirstname: false,
+    showDescription: false
+  }
 
   render() {
     const { data } = this.props;
+    const { showFirstname, showLastname, showDescription } = this.state;
 
     return (
       <section className={styles.container}>
+        <CSSTransition in={true} appear={true} classNames='slide' timeout={1000}>
           <div className={`${styles.textContainer}`} >
-            <h2 className={typography.display1}>{data.first}</h2>
+            <h2 className={typography.display1} >{data.first}</h2>
             <h2 className={typography.display1}>{data.second}</h2>
           </div>
-          <div className={styles.descriptionContainer}>
-            {/* <h3 className={typography.display1}>}</h3> */}
-          </div>
+        </CSSTransition>
+        <div className={styles.descriptionContainer}>
+          <h3 className={typography.display1}>}</h3>
+        </div>
       </section>
     )
   }
